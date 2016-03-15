26dd927f3a7d148084e5fee61c6e530062d5a07b
webdesign <webdesign>
2016-03-14 18:00:37 -0700

    new

--- draggable.js
+++ draggable.js
@@ -14,7 +14,7 @@ var Draggable = function( param ) {
 
 	var Draggable = {},
 	_el,
-	_flag         = false,
+	_flag         = { drag : false, toggle : false },
 	_callbacks    = { 
 
 		move : function( a, b ){ console.log( 'x: ' + a + ' y: ' + b + ' mouse moving.' ); },
@@ -22,7 +22,16 @@ var Draggable = function( param ) {
 		up   : function( a, b ){ console.log( 'x: ' + a + ' y: ' + b + ' mouse up' ); },
 		down : function( a, b ){ console.log( 'x: ' + a + ' y: ' + b + ' mouse down' ); }
 		
-	}
+	},
+	_temp = { 
+
+		move: function(){},
+		drag: function(){},
+		up  : function(){},
+		down: function(){}
+
+	},
+	_global = { node: null };
 	
 	/**
 	 * Add events done here.
@@ -54,11 +63,11 @@ var Draggable = function( param ) {
 				var x = e.clientX - this.offsetLeft, 
 				y 	  = e.clientY - this.offsetTop;
 
-				_callbacks[ 'move' ]( x, y, this );
+				_callbacks[ 'move' ]( x, y, this, _global );
 
-				if( _flag ) {
+				if( _flag.drag ) {
 
-					_callbacks[ 'drag' ]( x, y, this );
+					_callbacks[ 'drag' ]( x, y, this, _global  );
 
 				}
 				
@@ -69,9 +78,9 @@ var Draggable = function( param ) {
 				var x = e.clientX - this.offsetLeft, 
 				y 	  = e.clientY - this.offsetTop;
 
-				_flag = false;
+				_flag.drag = false;
 
-				_callbacks[ 'up' ]( x, y, this );
+				_callbacks[ 'up' ]( x, y, this, _global  );
 
 			});
 
@@ -80,9 +89,9 @@ var Draggable = function( param ) {
 				var x = e.clientX - this.offsetLeft, 
 				y 	  = e.clientY - this.offsetTop;
 
-				_flag = true;
+				_flag.drag = true;
 
-				_callbacks[ 'down' ]( x, y, this );
+				_callbacks[ 'down' ]( x, y, this, _global  );
 
 			});
 
@@ -130,10 +139,152 @@ var Draggable = function( param ) {
 
 		}
 
-	
+	}
+
+	Draggable.toggle = function( a ) {
+
+		if( a === undefined ) {
+
+			return 	_flag.toggle;
+
+		}
+
+		if( a ) {
+
+			if( !_flag.toggle ) {
+
+				var temp     = _callbacks;
+				_callbacks   = _temp;
+				_temp        = temp;
+				_flag.toggle = true;
+
+			}
+
+		} else {
+			
+			if( _flag.toggle ) {
+
+				var temp     = _callbacks;
+				_callbacks   = _temp;
+				_temp        = temp;
+				_flag.toggle = false;
+
+			}
+		}
 
 	}
 
+	Draggable.move = function( a ) {
+
+		_callbacks[ 'move' ] = a ;
+
+		return this;
+
+	}
+
+	Draggable.drag = function( a ) {
+
+		_callbacks[ 'drag' ] = a ;
+
+		return this;
+		
+	}
+
+	Draggable.down = function( a ) {
+
+		_callbacks[ 'down' ] = a ;
+
+		return this;
+		
+	}
+
+	Draggable.up = function( a ) {
+
+		_callbacks[ 'up' ] = a ;
+
+		return this;
+		
+	}
+
 	return init();
+
+}
+
+var List = function() {
+
+	var List = {},
+	nodes    = [];
+
+	List.add = function( a, b ) {
+
+		nodes[ a ] = b;
+
+	}
+
+	List.get = function( a ) {
+
+		return nodes[ a ];
+
+	}
+
+	List.nodes = function() {
+
+		return nodes;
+
+	}
+
+	List.size = function() {
+
+		console.log( nodes.length );
+		return nodes.length;
+
+	}
+
+	return List;
+
+}
+
+var DraggableManager = function() {
+
+	var DraggableManager = new List();
+
+	DraggableManager.toggle = function( a ) {
+
+		var draggables = this.nodes();
+
+		for( var key in draggables ) {
+
+			if( key == a ) {
+
+				draggables[ key ].toggle( false );
+				continue;
+
+			} 
+
+			if( draggables[ key ] !== undefined ) {
+
+				draggables[ key ].toggle( true );
+
+			}
+
+		}
+
+	}
+
+	return DraggableManager;
+
+}();
+
+var DraggableFactory = {
+
+	new : function( param ) {
+
+		var temp = new Draggable({ el: param.node });
+
+		DraggableManager.add( param.name, temp );
+
+		return temp;
+
+	}
 
 }
